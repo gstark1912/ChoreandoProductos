8a6120af95c2f7234fbfecb61aad5cd88ac857f7
gstark1912 <gstark1912@gmail.com>
2015-07-28 23:32:20 -0300

    #Consola Jumbo

---
+++ ConsolaJumbo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsolaJumbo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 443431;
+            SaveArticleData(num);
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+
+        private static void SaveArticleData(int num)
+        {
+            // HTTP POST
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.jumbo.com.ar/Comprar/HomeService.aspx/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var data = new A { code = num.ToString() };
+            HttpResponseMessage response = client.PostAsJsonAsync("ObtenerDetalleDelArticuloLevex", data).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content;
+                result = null;
+            }
+        }
+    }
+
+    class A
+    {
+        public string code { get; set; }
+    }
+}
