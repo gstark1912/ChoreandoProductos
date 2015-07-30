9e6b7d9726d375dd0c03e8d50119d9460bd691ca
gstark1912 <gstark1912@gmail.com>
2015-07-30 11:48:09 -0300

    WallMart

--- ConsolaJumbo/Program.cs
+++ ConsolaJumbo/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
 
 namespace ConsolaJumbo
 {
@@ -22,24 +25,26 @@ namespace ConsolaJumbo
 
         private static void SaveArticleData(int num)
         {
+
             // HTTP POST
             using (var client = new HttpClient())
             {
                 //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
-                var myObject = new A();
-                myObject.textoBusqueda = "a";
-                myObject.IdMenu = "";
-                myObject.marca = "";
-                myObject.pager = "";
-                myObject.precioHasta = "";
-                myObject.precioDesde = "";
-                myObject.producto = "";
-                myObject.ordenamiento = 0;
+                var myObject = new A
+                    {
+                        textoBusqueda = "a",
+                        IdMenu = "",
+                        marca = "",
+                        pager = "",
+                        precioHasta = "",
+                        precioDesde = "",
+                        producto = "",
+                        ordenamiento = 0
+                    };
 
-                
                 StringContent data = new StringContent(JsonConvert.SerializeObject(myObject).ToString(), Encoding.UTF8, "application/json");
-                var response = client.PostAsJsonAsync("https://www.jumbo.com.ar/Comprar/HomeService.aspx/ObtenerArticulosPorDescripcionMarcaFamiliaLevex", data).Result;
+                var response = client.PostAsJsonAsync("https://www.jumbo.com.ar/Comprar/HomeService.aspx/ObtenerArticulosPorDescripcionMarcaFamiliaLevex", myObject).Result;
                 if (response.IsSuccessStatusCode)
                 {
                     var result = response.Content;
@@ -63,7 +68,7 @@ namespace ConsolaJumbo
         public string producto { get; set; }
         public string marca { get; set; }
         public string pager { get; set; }
-        public int ordenamiento { get; set; }
+        public Byte ordenamiento { get; set; }
         public string precioDesde { get; set; }
         public string precioHasta { get; set; }
     }
---
+++ ConsolaWaltMart/Program.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace ConsolaWaltMart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "C:\\WalMart\\";
+            var allFilenames = Directory.EnumerateFiles(path).Select(p => Path.GetFileName(p)).Where(f => !f.Contains("_done"));
+            int i = 0;
+            foreach (var file in allFilenames)
+            {
+                using (var reader = new StreamReader(path + file))
+                {
+                    string line = reader.ReadToEnd();
+                    dynamic dynObj2 = new JavaScriptSerializer().Deserialize<List<object>>(line);
+                    foreach (var item in dynObj2)
+                    {
+                        SaveObject(item);
+                        i++;
+                    }
+                }
+                System.IO.File.Move(path + file, path + "_done_" + file);
+
+            }
+
+            if (allFilenames.Count() == 0)
+            {
+                allFilenames = Directory.EnumerateFiles(path).Select(p => Path.GetFileName(p));
+                foreach (var file in allFilenames)
+                {
+                    System.IO.File.Move(path + file, path + file.Replace("_done_", ""));
+                }
+            }
+
+            Console.WriteLine("Finished" + i);
+            Console.ReadLine();
+        }
+
+        private static void SaveObject(dynamic item)
+        {
+            ProductoWallmart entity = new ProductoWallmart
+            {
+                Descripcion = item["Description"],
+                Precio = item["Precio"],
+                UPC = item["upc"]
+            };
+            entity.Descripcion = entity.Descripcion.Trim();
+            entity.Precio = entity.Precio.Trim();
+            entity.UPC = entity.UPC.Trim();
+
+            Console.WriteLine(entity.Descripcion);
+            Console.WriteLine(entity.Precio);
+            Console.WriteLine(entity.UPC);
+
+            SaveP(entity);
+        }
+
+        private static void SaveP(ProductoWallmart entity)
+        {
+            using (var db = new ProductosEntities())
+            {
+                db.ProductoWallmart.Add(entity);
+                db.SaveChanges();
+            }
+        }
+    }
+
+}
